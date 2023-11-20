fc2454cdc0cfc893cc536cebdcda51374cd8a50f
Kentoes PDAM <108379342+kentoespdam@users.noreply.github.com>
2023-11-20 13:07:29 +0700

    add view file dialog

--- src/components/trans/admin/detail/kinerja/index.tsx
+++ src/components/trans/admin/detail/kinerja/index.tsx
@@ -4,6 +4,10 @@ import TableContainer from "@mui/material/TableContainer";
 import KpiAdminKinerjaTableBody from "./table/body";
 import KpiAdminKinerjaTableFooter from "./table/footer";
 import KpiAdminKinerjaTableHead from "./table/header";
+import { useQueryClient } from "@tanstack/react-query";
+import TableLoading from "@components/commons/table/loading";
+import ViewFileDialog from "@components/trans/dialog/file";
+import ViewPdfDialog from "@components/trans/dialog/pdf";
 
 export type KpiAdminKinerjaProps = {
 	periode: Periode | null;
@@ -13,21 +17,45 @@ export type KpiAdminKinerjaProps = {
 const KpiAdminKinerja = (props: KpiAdminKinerjaProps) => {
 	const { nipam, kpiId, periode } = props;
 
+	const qc = useQueryClient();
+	const qstate = qc.getQueryState([
+		"kpi.admin.kinerja",
+		{ nipam: nipam, kpiId: kpiId, periode: periode?.periode },
+	]);
+
 	return (
 		<TableContainer>
 			<Table>
 				<KpiAdminKinerjaTableHead />
-				<KpiAdminKinerjaTableBody
-					nipam={nipam}
-					kpiId={kpiId}
-					periode={periode}
-				/>
+				{qstate?.fetchStatus === "fetching" ? (
+					<TableLoading colSpan={14} />
+				) : qstate?.error ? (
+					<TableLoading colSpan={14} error />
+				) : (
+					<KpiAdminKinerjaTableBody
+						nipam={nipam}
+						kpiId={kpiId}
+						periode={periode}
+					/>
+				)}
 				<KpiAdminKinerjaTableFooter
 					nipam={nipam}
 					kpiId={kpiId}
 					periode={periode}
 				/>
 			</Table>
+
+			<ViewFileDialog
+				qKeyKpiStaff={[
+					"kpi.admin.kinerja",
+					{
+						nipam: nipam ?? null,
+						kpiId: kpiId,
+						periode: periode?.periode,
+					},
+				]}
+			/>
+			<ViewPdfDialog />
 		</TableContainer>
 	);
 };
