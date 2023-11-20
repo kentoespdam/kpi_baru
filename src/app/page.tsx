85ca96deff59c3652609b9b5f41604d9de4c98f9
Kentoes PDAM <108379342+kentoespdam@users.noreply.github.com>
2023-11-20 07:42:50 +0700

    use next_public_url in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { cookies } from "next/dist/client/components/headers";
 import { redirect } from "next/navigation";
-import { APP_URL, sessionNames } from "src/lib";
+import { sessionNames } from "src/lib";
 
 export default function Home() {
 	const cookie = cookies();
 	const isLogin = cookie.has(sessionNames[0]) || cookie.has(sessionNames[1]);
 
-	if (isLogin) redirect(`${APP_URL}/trans/kpi`);
-	else redirect(`${APP_URL}/login`);
+	if (isLogin) redirect(`${process.env.NEXT_PUBLIC_URL}/trans/kpi`);
+	else redirect(`${process.env.NEXT_PUBLIC_URL}/login`);
 }
