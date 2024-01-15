6793ef08339330454c7b04ad53281b67ab03e52a
Kentoes PDAM <108379342+kentoespdam@users.noreply.github.com>
2024-01-15 16:22:59 +0700

    first-commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Auth = () => {
+	return (
+		<section className="bg-gray-50 dark:bg-gray-900">
+			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+				<Link
+					href="#"
+					className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+				>
+					<Image
+						className="w-8 h-8 mr-2"
+						src="/logo_pdam_40x40.png"
+						width={40}
+						height={40}
+						alt="logo"
+					/>
+					Perumdam Tirta Satria
+				</Link>
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+							Sign in to your account
+						</h1>
+						<form className="space-y-4 md:space-y-6" action="#">
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Your email
+								</label>
+								<Input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="password"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Password
+								</label>
+								<Input
+									type="password"
+									name="password"
+									id="password"
+									placeholder="••••••••"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
+								/>
+							</div>
+
+							<Button className="w-full focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+								Sign in
+							</Button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Auth;
